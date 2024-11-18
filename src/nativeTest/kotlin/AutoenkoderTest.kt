36b50a06ad8106a9dd0ceec652bfdd382d40d9be
Kenny Cason <kenneth.cason@gmail.com>
2024-11-17 21:12:47 -0800

    add test for larger random vectors

--- src/nativeTest/kotlin/AutoenkoderTest.kt
+++ src/nativeTest/kotlin/AutoenkoderTest.kt
@@ -1,4 +1,5 @@
 import kotlin.math.ceil
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.time.measureTime
 
@@ -26,6 +27,25 @@ class AutoenkoderTest {
         }
     }
 
-   private fun round(value: Double) = ceil(value * 10) / 10
+    @Test
+    fun `autoencoder - random vectors`() {
+        val autoEncoder = Autoenkoder(inputSize = 10, hiddenSize = 3)
+        val trainingData = Array(4) {
+            DoubleArray(10) { Random.nextDouble(0.0, 1.0) }
+        }
+        val elapsedMs = measureTime {
+            autoEncoder.train(trainingData, learningRate = 0.2, epochs = 10_000)
+        }.inWholeMilliseconds
+        println("trained in ${elapsedMs}ms")
+        trainingData.forEach { xs ->
+            println("input: ${xs.joinToString()}, reconstructed: ${autoEncoder.predict(xs).joinToString()}")
+        }
+        println("rounded")
+        trainingData.forEach { xs ->
+            println("input: ${xs.joinToString { round(it).toString() }}, reconstructed: ${autoEncoder.predict(xs).joinToString { round(it).toString() }}")
+        }
+    }
+
+    private fun round(value: Double) = ceil(value * 10) / 10
 
 }
